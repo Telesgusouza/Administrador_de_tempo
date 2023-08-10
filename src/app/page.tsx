a7d380e444ba09837c0ef213660e9283e00b8ddd
Gustavo Teles <gustavo.teles711@gmail.com>
2023-08-09 22:34:51 -0300

    commit inicial

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from './styles/page.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>querida cheguei</h1>
+    </div>
+  )
+}
